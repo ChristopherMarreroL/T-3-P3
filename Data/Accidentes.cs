9dfc5a3870dba76baf96a6f5b04ce7bf98db6ebf
ChristopherMarrereoL <christophermarrero35@gmail.com>
2023-11-25 23:33:36 -0500

    Primer archivo

---
+++ Data/Accidentes.cs
@@ -0,0 +1,8 @@
+public class Accidente{
+    public DateTime Fecha {get; set;}
+    public string Descripcion {get; set;}="";
+    public decimal CostoEconomico {get; set;}
+    public int Muertos {get; set;}
+    public int Heridos {get; set;}
+    public int CantidadVehiculos {get; set;}
+}
